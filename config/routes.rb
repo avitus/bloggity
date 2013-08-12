7aac91c1624e8c18b3269e393cdec78d2f3136cc
Andy <avitus@gmail.com>
2013-08-12 16:19:48 -0700

    Modernize route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Bloggity::Engine.routes.draw do
 
 	resources :blogs do
 
-		resources :blog_posts do     
-		  
+		resources :blog_posts do
+
 			collection do
 				get :pending
 				post :create_asset
@@ -21,19 +21,19 @@ Bloggity::Engine.routes.draw do
 			get :feed
 		end
 
-	end	
-	
+	end
+
 	resources :blog_categories
 	resources :blog_assets
 	resources :blog_comments, :member => { :approve => :get }
 
 
 	# Blog routes
-	match '/blog_comments_new',      :to => 'blog_comments#recent_comments',                   :as => 'blog_comments_new'
-	match '/blog_search',            :to => 'blog_posts#blog_search',                          :as => 'blog_search'
+	get '/blog_comments_new',      :to => 'blog_comments#recent_comments',                   :as => 'blog_comments_new'
+	get '/blog_search',            :to => 'blog_posts#blog_search',                          :as => 'blog_search'
 
-	match  ':blog_url_id_or_id',     :to => 'blog_posts#index'
-	match  ':blog_url_id_or_id/:id', :to => 'blog_posts#show'
-	match  'blog',                   :to => 'blog_posts#index', :as => 'blog', :blog_url_id_or_id => 'main'
+	get  ':blog_url_id_or_id',     :to => 'blog_posts#index'
+	get  ':blog_url_id_or_id/:id', :to => 'blog_posts#show'
+	get  'blog',                   :to => 'blog_posts#index', :as => 'blog', :blog_url_id_or_id => 'main'
 
 end
