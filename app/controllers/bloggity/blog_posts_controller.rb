b19566da5dc1c3ab773bb7e2ba396896763ba45e
Andy <avitus@gmail.com>
2013-08-15 10:44:56 -0700

    Clean up old query syntax

--- app/controllers/bloggity/blog_posts_controller.rb
+++ app/controllers/bloggity/blog_posts_controller.rb
@@ -1,49 +1,49 @@
 module Bloggity
   class BlogPostsController < Bloggity::ApplicationController
-    
+
     include Bloggity::ApplicationHelper
 
     # ALV: The page name doesn't seem to get used anywhere
     # before_filter :get_bloggity_page_name, :except => :blog_search
-    
+
     before_filter :load_blog_post, :except => :blog_search
     before_filter :blog_writer_or_redirect, :except => [:close, :index, :show, :feed, :blog_search]
-  	
+
     # GET /blog_posts
     # GET /blog_posts.xml
     add_breadcrumb "Home", "/home"
     add_breadcrumb "Blog", "/blog"
 
     def index
-      
+
       @tab = "blog"
       @sub = "view"
 
   		@blog_page = params[:page] || 1
   	  @recent_posts = recent_posts(@blog_page)
   		@blog_posts = if(params[:tag_name] || params[:category_id])
-  			# This is how I'd *like* to filter on tag/category:  
+  			# This is how I'd *like* to filter on tag/category:
   			#search_condition = { :blog_id => @blog_id, :is_complete => true }
   			#search_condition.merge!(:blog_tags => { :name => params[:tag_name] }) if params[:tag_name]
-  			# However something about the interaction of these options is terminally dumb, as the include or join 
+  			# However something about the interaction of these options is terminally dumb, as the include or join
   			# is ignored by the test suite or script/server, respectively, when using this logic.
-  			
+
   			# So alas... we must hack away:
   			search_condition = ["blog_id = ? AND is_complete = ? #{"AND bloggity_blog_tags.name = ?" if params[:tag_name]} #{"AND category_id = ?" if params[:category_id]}", @blog_id, true, params[:tag_name], params[:category_id]].compact
-        BlogPost.where(search_condition).select("DISTINCT blog_posts.*").includes(:tags).page(@blog_page).order("bloggity_blog_posts.created_at DESC")      
+        BlogPost.where(search_condition).select("DISTINCT blog_posts.*").includes(:tags).page(@blog_page).order("bloggity_blog_posts.created_at DESC")
   		else
   			logger.info("*** Showing recent posts ")
   			@recent_posts
   		end
-  			
+
   		@page_name = @blog.title
-  	    
+
   		respond_to do |format|
   	      format.html # index.html.erb
   	      format.xml  { render :xml => @blog_posts }
       end
     end
-  	
+
   	def close
   		@blog_post = BlogPost.find(params[:id])
       if blog_logged_in? && current_user.can_moderate_blog_comments?(@blog_id)
@@ -63,19 +63,19 @@ module Bloggity
   		@image.save!
   		render :text => @image.public_filename
   	end
-  	
+
     # GET /blog_posts/1
     # GET /blog_posts/1.xml
     def show
       @tab = "blog"
       @blog_page = params[:page] || 1
-      
+
       Rails.logger.info("*** Fetching recent posts for blog page #{@blog_page}")
-      
+
       @recent_posts = recent_posts(@blog_page)
-      
+
       @blog_post = BlogPost.where("id = ? OR url_identifier = ?", params[:id], params[:id]).first
-      
+
       if !@blog_post || (!@blog_post.is_complete  && (!current_user || !current_user.can_blog?(@blog_post.blog_id)))
         @blog_post = nil
         flash[:error] = "You do not have permission to see that blog post."
@@ -92,10 +92,10 @@ module Bloggity
             flash.keep[:notice] = "You have completed the task: #{q.task}"
           end
         end
-        # end        
-        
+        # end
+
       end
-  	
+
       respond_to do |format|
         format.html # show.html.erb
         format.xml  { render :xml => @blog_post }
@@ -135,7 +135,7 @@ module Bloggity
     # PUT /blog_posts/1.xml
     def update
       @blog_post = BlogPost.find(params[:id])
-      
+
       if @blog_post.update_attributes(params[:blog_post])
         redirect_to blog_named_link(@blog_post)
       else
@@ -151,35 +151,35 @@ module Bloggity
       flash[:message] = "Blog #{@blog_post.title} was destroyed."
       redirect_to(blog_named_link(nil, :index, { :blog => @blog }))
     end
-  	
+
   	def pending
-      @tab = "blog" 
+      @tab = "blog"
       blog_page = params[:page] || 1
   		@pending_posts = BlogPost.where(:blog_id => @blog_id, :is_complete => false).page(@blog_page).order("created_at DESC")
       @recent_posts = recent_posts(blog_page)
   	end
-  	
-  	
+
+
     # ----------------------------------------------------------------------------------------------------------
     # Blog Search Results
-    # ----------------------------------------------------------------------------------------------------------  	
-  	def blog_search_results	  
+    # ----------------------------------------------------------------------------------------------------------
+  	def blog_search_results
   		@tab = "blog"
       @sub = "blogsearch"
       @blog_search_results = BlogPost.search( params[:search_param] )
-      respond_to do |format| 
+      respond_to do |format|
         format.html { render :partial => 'blog_search_results', :layout=>false }
         format.xml  { render :xml 	=> @blog_search_results }
-      end	    
+      end
   	end
 
 
     # ----------------------------------------------------------------------------------------------------------
     # Blog Search Query page
-    # ---------------------------------------------------------------------------------------------------------- 	
+    # ----------------------------------------------------------------------------------------------------------
   	def blog_search
       @tab = "blog"
-      @sub = "blogsearch"		
+      @sub = "blogsearch"
   		@blog_search_results = Array.new
   	  @blog_search_results = BlogPost.search( params[:search_param] || "memorize" )
   	end
@@ -189,12 +189,12 @@ module Bloggity
   	private
   	# --------------------------------------------------------------------------------------
   	# --------------------------------------------------------------------------------------
-  	
+
   	def load_blog_post
   		load_blog
-  		@blog_post = BlogPost.find(:first, :conditions => ["blog_id = ? AND (id = ? OR url_identifier = ?)", @blog_id, params[:id], params[:id]]) if params[:id]
+  		@blog_post = BlogPost.where("blog_id = ? AND (id = ? OR url_identifier = ?)", @blog_id, params[:id], params[:id]).first if params[:id]
   	end
-  	
+
   	def recent_posts(blog_page)
   		Rails.logger.debug("*** Loading recent posts for blog with id: #{@blog_id}")
       BlogPost.where(:blog_id => @blog_id, :is_complete => true).page(@blog_page).order("created_at DESC")
